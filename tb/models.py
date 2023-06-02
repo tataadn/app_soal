1a5559ea293d24126beb743688eb6bfa7d0b0c1b
tataadn <adine.pramesti@gmail.com>
2023-06-02 21:54:10 +0700

    backup, new db, backup tb_user

--- tb/models.py
+++ tb/models.py
@@ -2,39 +2,41 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-# class Soal(models.Model):
-#     id_soal = models.AutoField(primary_key=True)
-#     id_kelas = models.CharField(max_length=10, null=False)
-#     id_mapel = models.CharField(max_length=10, null=False)
-#     kode_soal = models.CharField(max_length=10, null=False)
-#     soal = models.TextField(null=False)
-#     kunci_jawaban = models.TextField(null=False)
-#     bobot_soal = models.IntegerField(null=False)
+class Soal(models.Model):
+    id_soal = models.CharField(primary_key=True, max_length=50)
+    id_kelas = models.CharField(max_length=10, null=False)
+    id_mapel = models.CharField(max_length=10, null=False)
+    kode_soal = models.CharField(max_length=50, null=False)
+    soal = models.TextField(null=False)
+    kunci_jawaban = models.TextField(null=False)
+    bobot_soal = models.IntegerField(null=False)
 
 class User(AbstractUser):
     nomor_induk = models.CharField(max_length=200, null=False)
     nama_lengkap = models.CharField(max_length=255, null=False)
     alamat = models.TextField(null=False)
     jenis_kelamin = models.CharField(max_length=10, null=False)
-    foto = models.ImageField(upload_to='foto/', null=True)
     is_siswa = models.BooleanField('Is siswa',default=False)
     is_guru = models.BooleanField('Is guru',default=False)
     is_admin = models.BooleanField('Is admin',default=False)
+    foto = models.ImageField(upload_to='foto/', null=True)
+    id_mapel = models.CharField(max_length=10)
+    id_kelas = models.CharField(max_length=10)
 
-# class Mapel(models.Model):
-#     id_mapel = models.AutoField(primary_key=True)
-#     nama_mapel = models.CharField(max_length=100, null=False)
+class Mapel(models.Model):
+    id_mapel = models.CharField(primary_key=True, max_length=50)
+    nama_mapel = models.CharField(max_length=100, null=False)
 
-# class Kelas(models.Model):
-#     id_kelas = models.AutoField(primary_key=True)
-#     nama_kelas = models.CharField(max_length=50, null=False)
+class Kelas(models.Model):
+    id_kelas = models.CharField(primary_key=True, max_length=50)
+    nama_kelas = models.CharField(max_length=50, null=False)
 
-# class Jawaban(models.Model):
-#     id_jawaban = models.AutoField(primary_key=True)
-#     id_soal = models.CharField(max_length=10, null=False)
-#     jawaban = models.TextField(null=False)
-#     nilai = models.IntegerField(null=False)
-
+class Jawaban(models.Model):
+    id_jawaban = models.AutoField(primary_key=True)
+    id_soal = models.CharField(max_length=50, null=False)
+    kode_soal = models.CharField(max_length=50, null=False)
+    jawaban = models.TextField(null=False)
+    nilai = models.IntegerField(null=False)
 
 # class Siswa(models.Model):
 #     nis_siswa = models.CharField(max_length=50, null=False)
---
+++ tb_user.sql
@@ -0,0 +1,54 @@
+/*
+SQLyog Ultimate v12.4.3 (64 bit)
+MySQL - 10.4.28-MariaDB : Database - db_smp
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_smp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `db_smp`;
+
+/*Table structure for table `tb_user` */
+
+DROP TABLE IF EXISTS `tb_user`;
+
+CREATE TABLE `tb_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `password` varchar(128) NOT NULL,
+  `last_login` datetime(6) DEFAULT NULL,
+  `is_superuser` tinyint(1) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `first_name` varchar(150) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `is_staff` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `date_joined` datetime(6) NOT NULL,
+  `nomor_induk` varchar(200) NOT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `alamat` longtext NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `is_siswa` tinyint(1) NOT NULL,
+  `is_guru` tinyint(1) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL,
+  `foto` varchar(100) DEFAULT NULL,
+  `id_mapel` varchar(10) NOT NULL,
+  `id_kelas` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tb_user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
